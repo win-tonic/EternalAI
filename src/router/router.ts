ed397e9e6d04a23700ffba508704dc96241f117f
kekwkekw <danulo2403@gmail.com>
2024-06-15 13:00:40 +0300

    error middleware added

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,24 +4,25 @@ import accountController from "../controllers/accountController";
 import chatController from "../controllers/chatController";
 import paymentController from "../controllers/paymentController";
 import { needsToken } from "../middleware/authMiddleware";
+import { errorMiddleware } from "../middleware/errorHandler";
 
 const router = Router();
 
-router.post('/signup', authController.signUp);
-router.post('/signin', authController.signIn);
-router.post('/googleAuth', authController.googleAuth);
+router.post('/signup', errorMiddleware(authController.signUp));
+router.post('/signin', errorMiddleware(authController.signIn));
+router.post('/googleAuth', errorMiddleware(authController.googleAuth));
 
-router.get('/accountInfo', needsToken(accountController.getAccountInfo));
-router.post('/changeAccountInfo', needsToken(accountController.changeInfo))
-router.post('/changePassword', needsToken(accountController.changePassword))
-router.post('/socialMediaShared', needsToken(accountController.giveThreeQuestions))
+router.get('/accountInfo', errorMiddleware(needsToken(accountController.getAccountInfo)));
+router.post('/changeAccountInfo', errorMiddleware(needsToken(accountController.changeInfo)))
+router.post('/changePassword', errorMiddleware(needsToken(accountController.changePassword)))
+router.post('/socialMediaShared', errorMiddleware(needsToken(accountController.giveThreeQuestions)))
 
-router.post('/newChat', needsToken(chatController.createNewChat));
-router.get('/chatInfo', needsToken(chatController.getChatInfo));
-router.post('/sendMessage', needsToken(chatController.sendMessage));
+router.post('/newChat', errorMiddleware(needsToken(chatController.createNewChat)));
+router.get('/chatInfo', errorMiddleware(needsToken(chatController.getChatInfo)));
+router.post('/sendMessage', errorMiddleware(needsToken(chatController.sendMessage)));
 
-router.post('/subscribe', needsToken(paymentController.createPaymentIntent));
-router.post('/unsubscribe', needsToken(paymentController.cancelSubscription));
-router.post('/stripe/webhook', paymentController.webhook);
+router.post('/subscribe', errorMiddleware(needsToken(paymentController.createPaymentIntent)));
+router.post('/unsubscribe', errorMiddleware(needsToken(paymentController.cancelSubscription)));
+router.post('/stripe/webhook', errorMiddleware(paymentController.webhook));
 
 export { router };
